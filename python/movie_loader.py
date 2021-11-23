4ba84b3a60cb0537ef43d693398ac40427b38bb1
cpon00 <cpon712@gmail.com>
2021-11-22 18:20:55 -0800

    changed loader.py to movie_loader.py, populated movies.csv successfully

--- python/movie_loader.py
+++ python/movie_loader.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import date
 import csv
 import json
 import os
+import sys
 # from neo4j import GraphDatabase
 
 # For simplicity, we assume that the program runs where the files are located.
@@ -11,6 +12,7 @@ CREDIT_SOURCE = 'tmdb_5000_credits.csv'
 DESTINATION = 'movies.csv'
 # Writes into destination
 post_processed_file = open(DESTINATION, 'w')
+writer = csv.writer(post_processed_file)
 current_movie_id = None
 
 # to connect to database
@@ -24,19 +26,18 @@ current_movie_id = None
 # We have two files to load, but only some of the files in the credits are relevant.
 # So we open both and use only the cast and crew portion of credits.
 
-with open(MOVIE_SOURCE, 'r+', encoding='UTF-8') as m, open(CREDIT_SOURCE, 'r+', encoding='UTF-8') as c:
-    movie_list, credit_list = list(csv.DictReader(m)), list(csv.DictReader(c))
-    for movie, credit in zip(movie_list, credit_list):
-
+with open(MOVIE_SOURCE, 'r+', encoding='UTF-8') as m:
+    movie_list = list(csv.DictReader(m))
+    for movie in movie_list:
         result = {}
         result['id'] = int(movie['id'])
         result['title'] = movie['title']
         # There were singular movies in our dataset that had bad values. As such, rather than simply removing them, we gave them an arbitrary date.
         if movie['release_date'] == '':
-            result['release_date'] = datetime.strptime('3000-1-1', '%Y-%m-%d')
+            result['release_date'] = date.fromisoformat('3000-1-1')
         else:
-            result['release_date'] = datetime.strptime(
-                movie['release_date'], '%Y-%m-%d')
+            result['release_date'] = date.fromisoformat(
+                movie['release_date'])
         result['original_language'] = movie['original_language']
         result['budget'] = int(movie['budget'])
 
@@ -55,13 +56,10 @@ with open(MOVIE_SOURCE, 'r+', encoding='UTF-8') as m, open(CREDIT_SOURCE, 'r+', 
 
         # result['cast'] = json.loads(credit['cast'])
         # result['crew'] = json.loads(credit['crew'])
-        # Instead of creating a results object, I could have built an object within the insert_one(), but I felt this was clearer and more concise. Pardon my space usage.
-        # x = db.movie.insert_one(result)
+        post_processed_file.write(f"{result['id']}|{result['title']}|{result['release_date']}|{result['original_language']}|" \
+                                  f"{result['budget']}|{result['popularity']}|{result['vote_average']}|{result['runtime']}\n")
 
-        print(result['id'], result['title'], result['release_date'],
-              result['original_language'], result['budget'], result['popularity'], result['vote_average'], result['runtime'], sep='|')
-
-# post_processed_file.close()
+post_processed_file.close()
 
 # Print statement for ID of inserted entry.
 # print(x.inserted_id)
