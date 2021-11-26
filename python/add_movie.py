810fa5b213b10d50d791ec3baea093475cd07d02
Josh Seaman <joshseaman@Joshs-MacBook-Pro.local>
2021-11-26 00:31:55 -0500

    worked on DAL

--- python/add_movie.py
+++ python/add_movie.py
@@ -1,17 +1,52 @@
 import sys
 
-from tmdb_dal import insert_movie
 
-if len(sys.argv) != 3:
-    print('Usage: add_movie <title> <year>')
-    exit(1)
+from tmdb_dal import insert_movie
+from tmdb_dal import insert_title
+
+# if len(sys.argv) != 9:
+#     print('Usage: add_movie <title> <date> <language> <budget> <popularity> <vote-average> <runtime> <id>')
+#     exit(1)
+
 
 title = sys.argv[1]
-year = sys.argv[2]
+print(title)
+
+#"1000-01-01"
+# date = sys.argv[2]
+# print(date)
+
+# lang = sys.argv[3]
+# print(lang)
+
+# budget = sys.argv[4]
+# print(budget)
+
+# popularity = sys.argv[5]
+# print(popularity)
+
+# vote_av = sys.argv[6]
+# print(vote_av)
+
+# runtime = sys.argv[7]
+# print(runtime)
+
+# movie_id = sys.argv[8]
+# print(movie_id)
+
+
+
+
+# "Josh", "2000-09-17", "en", 10000, 10, 100, 1000
+
 try:
-    movie = insert_movie(title, int(year))
+    movie = insert_title(title)
+    # movie = insert_movie(title, date, lang, int(budget), float(popularity), float(vote_av), int(runtime), int(movie_id))
     print(
-        f"Movie “{movie.get('title')}” ({movie.get('year')}) added with ID {movie.id}.")
+        f"Movie “{movie.get('title')}” added with")
 except ValueError:
     print(
-        f'Sorry, something went wrong. Please ensure that “{year}” is a valid year.')
+        f'Sorry, something went wrong.')
+
+
+# movieId:id(Movie)|title:STRING|release_date:DATE|original_language:STRING|budget:INT|popularity:FLOAT|vote_average:FLOAT|runtime:INT
--- python/tmdb_dal.py
+++ python/tmdb_dal.py
@@ -13,66 +13,95 @@ db = GraphDatabase.driver(os.environ['DB_URL'], auth=(
     db_user, os.environ['DB_PASSWORD']))
 
 
-def search_movies_by_title(title_query, limit=100):
+# def search_movies_by_title(title_query, limit=100):
+#     with db.session() as session:
+#         result = session.run(
+#             """
+#             MATCH (m:Movie)
+#             WHERE toLower(m.title) CONTAINS toLower($title_query)
+#             RETURN m
+#             ORDER BY m.title
+#             LIMIT $limit
+#             """,
+#             title_query=title_query,  # The `query` parameter name is already taken.
+#             limit=limit)
+
+#         # The result needs to be consumed while the session is open.
+#         return [record.get('m') for record in result]
+
+
+# def get_average_rating_of_movie(movie_id):
+#     with db.session() as session:
+#         result = session.run(
+#             """
+#             MATCH (m:Movie)<-[r:RATED]-(:Viewer)
+#             WHERE (exists(m.movieId) AND m.movieId = $movie_id) OR
+#                   (NOT exists(m.movieId) AND id(m) = $identity)
+#             RETURN avg(r.rating)
+#             """,
+#             movie_id=str(
+#                 movie_id),  # movie_id is passed as an int so we need to convert to a string here.
+#             identity=movie_id)
+
+#         # We know in advance that this will be a single record so we feel safe about hardcoding this.
+#         # A non-existent movie will yield `None` for this expression.
+#         return result.single().get('avg(r.rating)')
+
+
+# def get_ratings_by_viewer(viewer_id, limit=100):
+#     with db.session() as session:
+#         result = session.run(
+#             """
+#             MATCH (viewer:Viewer {viewerId: $viewer_id})-[rating:RATED]->(movie:Movie)
+#             RETURN viewer, rating, movie
+#             ORDER BY rating.dateRated, movie.title
+#             LIMIT $limit
+#             """,
+#             viewer_id=viewer_id,
+#             limit=limit)
+
+#         # The result needs to be consumed while the session is open.
+#         return [record for record in result]
+
+# movieId:id(Movie)|title:STRING|release_date:DATE|original_language:STRING|budget:INT|popularity:FLOAT|vote_average:FLOAT|runtime:INT
+
+def insert_movie(id, title, date, lang, budget, popularity, vote_av, runtime):
     with db.session() as session:
         result = session.run(
             """
-            MATCH (m:Movie)
-            WHERE toLower(m.title) CONTAINS toLower($title_query)
-            RETURN m
-            ORDER BY m.title
-            LIMIT $limit
-            """,
-            title_query=title_query,  # The `query` parameter name is already taken.
-            limit=limit)
-
-        # The result needs to be consumed while the session is open.
-        return [record.get('m') for record in result]
-
-
-def get_average_rating_of_movie(movie_id):
-    with db.session() as session:
-        result = session.run(
-            """
-            MATCH (m:Movie)<-[r:RATED]-(:Viewer)
-            WHERE (exists(m.movieId) AND m.movieId = $movie_id) OR
-                  (NOT exists(m.movieId) AND id(m) = $identity)
-            RETURN avg(r.rating)
-            """,
-            movie_id=str(
-                movie_id),  # movie_id is passed as an int so we need to convert to a string here.
-            identity=movie_id)
-
-        # We know in advance that this will be a single record so we feel safe about hardcoding this.
-        # A non-existent movie will yield `None` for this expression.
-        return result.single().get('avg(r.rating)')
-
-
-def get_ratings_by_viewer(viewer_id, limit=100):
-    with db.session() as session:
-        result = session.run(
-            """
-            MATCH (viewer:Viewer {viewerId: $viewer_id})-[rating:RATED]->(movie:Movie)
-            RETURN viewer, rating, movie
-            ORDER BY rating.dateRated, movie.title
-            LIMIT $limit
-            """,
-            viewer_id=viewer_id,
-            limit=limit)
-
-        # The result needs to be consumed while the session is open.
-        return [record for record in result]
-
-
-def insert_movie(title, year):
-    with db.session() as session:
-        result = session.run(
-            """
-            CREATE (insertedMovie:Movie {title: $title, year: $year})
+            CREATE (insertedMovie:Movie {movieId: $id, title: $title, release_date: $date, original_language: $lang, budget: $budget, popularity: $popularity, vote_average: $vote_av, runtime: $runtime})
             RETURN insertedMovie
             """,
-            title=title,
-            year=year)
+            # id=id,
+            # title=title,
+            # date=date,
+            # lang=lang,
+            # budget=budget,
+            # popularity=popularity,
+            # vote_av=vote_av,
+            # runtime=runtime
+            )
+
+        # This returns the full node so we have its identity and labels.
+        return result.single().get('insertedMovie')
+
+
+def insert_title(title):
+    with db.session() as session:
+        result = session.run(
+            """
+            CREATE (insertedMovie:Movie { title: $title})
+            RETURN insertedMovie
+            """
+            # id=id,
+            # title=title,
+            # date=date,
+            # lang=lang,
+            # budget=budget,
+            # popularity=popularity,
+            # vote_av=vote_av,
+            # runtime=runtime
+            )
 
         # This returns the full node so we have its identity and labels.
         return result.single().get('insertedMovie')
